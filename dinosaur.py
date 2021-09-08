84b53c1ef7fb9748b16c3f72cd26a6e966e2d4d9
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-08 18:17:20 -0400

    created init dino class w/name, health, and attack power

---
+++ dinosaur.py
@@ -0,0 +1,11 @@
+class Dinosaur:
+  def __init__ (self, dinosaur_name):
+    self.dinosaur_name = dinosaur_name
+    self.dinosaur_health = 10
+    self.dinosaur_attack_power = 4
+  
+
+# dino_one = Dinosaur("Test-Dino")
+# print(dino_one.dinosaur_name)
+# print(dino_one.dinosaur_attack_power)
+# print(dino_one.dinosaur_health)
