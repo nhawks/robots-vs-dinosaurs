05eed2c18cdf3c085630f1d485a315a11975b3ae
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-08 17:29:01 -0400

    created Weapon class for one weapon and added attack power

---
+++ weapon.py
@@ -0,0 +1,13 @@
+class Weapon:
+  def __init__ (self, weapon_name):
+      self.weapon_name = weapon_name
+      self.attack_power = self.weapon_power(weapon_name)
+
+
+  def weapon_power (self, weapon_name):
+    if weapon_name == "sword":
+      attack_power = 2
+      return attack_power
+
+
+Weapon("sword")
