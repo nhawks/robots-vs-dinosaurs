29b2443d770b95b1166099c8009340a621d674a0
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-08 20:01:47 -0400

    added list that will populate created robots and created three robot objects

--- robot.py
+++ robot.py
@@ -1,14 +1,19 @@
 from weapon import Weapon
 
 class Robot:
+  
+  #will be used to copy list to Fleet.robot_fleet
+  created_robots = []
+
   def __init__ (self, robot_name):
     self.robot_name = robot_name
     self.robot_health = 10
     self.robot_weapon = Weapon("sword")
 
+    #add each robot object to created_robots
+    Robot.created_robots.append(self)
 
-# robot_one = Robot("Test-X")
-# print(robot_one.robot_name)
-# print(robot_one.robot_health)
-# print(robot_one.robot_weapon.weapon_name)
-# print(robot_one.robot_weapon.attack_power)
+#Instance Robot Objects
+robot_one = (Robot("Vanquish-X4"))
+robot_two = (Robot("Neon-VX4"))
+robot_three = (Robot("Leo-BE3"))
