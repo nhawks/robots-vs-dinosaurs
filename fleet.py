d7bd196a3b7c223bb6debbce61b8e865cd4ee7d6
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-08 23:17:38 -0400

    added void function create_fleet

--- fleet.py
+++ fleet.py
@@ -3,8 +3,12 @@ from robot import Robot
 
 class Fleet:
 
-  #copy created_robots list
+  #list of created robots copied from Robot.created_robots
   robot_fleet = Robot.created_robots.copy()
 
   def __init__ (self):
-    self.create_fleet(self)
+    pass
+ 
+  def create_fleet (self): #void
+    pass
+  #end of class Fleet
