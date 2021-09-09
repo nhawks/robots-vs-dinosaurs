1dd2d536f756d291e9501e0882bbcb3ccee36690
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-08 22:53:09 -0400

    added attack method in class

--- robot.py
+++ robot.py
@@ -1,4 +1,6 @@
+import random
 from weapon import Weapon
+from herd import Herd
 
 class Robot:
   
@@ -12,6 +14,14 @@ class Robot:
 
     #add each robot object to created_robots
     Robot.created_robots.append(self)
+
+  #select a random dinosaur for opponent then attack    
+  def attack(self, dinosaur):
+    dinosaur = random.choice(Herd.dinosaur_herd)
+    print(f"Opponent: {dinosaur.dionsaur.firstname}")
+    dinosaur.dinosaur_health -= self.robot_weapon.weapon_damage
+  #end of class Robot 
+  
 
 #Instance Robot Objects
 robot_one = (Robot("Vanquish-X4"))
