99c9e3a852df099f73a343a6cd53e3a37b4b6a36
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-09 11:46:31 -0400

    added todo for futre weapons list

--- weapon.py
+++ weapon.py
@@ -3,9 +3,10 @@ class Weapon:
       self.weapon_name = weapon_name
       self.weapon_damage = self.get_weapon_damage(weapon_name)
 
-
+  
   def get_weapon_damage (self, weapon_name):
     if weapon_name == "sword":
       weapon_damage = 2
       return weapon_damage
-      ##*end of class Weapon
+  
+  #TODO: create list of different weapons: [sword, laser, handcannon] 
