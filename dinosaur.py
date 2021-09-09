366d783f652f3e06660a2cd0f3c8e0f42aa956ad
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-08 20:11:32 -0400

    created list that will add created dinosaurs each time the instance is called

--- dinosaur.py
+++ dinosaur.py
@@ -1,11 +1,17 @@
 class Dinosaur:
+
+  #will be used to copy list to Herd.dinosaur_herd
+  created_dinosaurs = []
+
   def __init__ (self, dinosaur_name):
     self.dinosaur_name = dinosaur_name
     self.dinosaur_health = 10
     self.dinosaur_attack_power = 4
-  
 
-# dino_one = Dinosaur("Test-Dino")
-# print(dino_one.dinosaur_name)
-# print(dino_one.dinosaur_attack_power)
-# print(dino_one.dinosaur_health)
+    #add each robot object to created_robots
+    Dinosaur.created_dinosaurs.append(self)
+
+#Instance Robot Objects
+dinosaur_one = Dinosaur("Pteranodon")
+dinosaur_two = Dinosaur("Cearadactylus")
+dinosaur_three = Dinosaur("Tropeognathus")
