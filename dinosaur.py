749b2ab10f99169b3cfa544bdb724f901253732c
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-09 12:04:36 -0400

    fixed class to match UML

--- dinosaur.py
+++ dinosaur.py
@@ -1,17 +1,10 @@
 class Dinosaur:
 
-  #will be used to copy list to Herd.dinosaur_herd
-  created_dinosaurs = []
-
-  def __init__ (self, dinosaur_name):
+  def __init__ (self, dinosaur_name, attack_power):
     self.dinosaur_name = dinosaur_name
+    self.attack_power = attack_power
     self.dinosaur_health = 10
-    self.dinosaur_attack_power = 4
 
-    #add each robot object to created_robots
-    Dinosaur.created_dinosaurs.append(self)
-
-#Instance Robot Objects
-dinosaur_one = Dinosaur("Pteranodon")
-dinosaur_two = Dinosaur("Cearadactylus")
-dinosaur_three = Dinosaur("Tropeognathus")
+  #attack selected robot
+  def attack (self, robot): #void
+    pass
