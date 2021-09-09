089798452921bfdedd13f503fe8b6ab5fe6463f6
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-08 23:00:04 -0400

    created init battlefield class

---
+++ battlefield.py
@@ -0,0 +1,33 @@
+from fleet import Fleet
+from herd import Herd
+
+class Battlefield:
+
+  def __init__ (self):
+    Fleet.robot_fleet
+    Herd.dinosaur_herd
+
+  def run_game(self): #void
+    pass
+
+  def display_welcome (self): #void
+    print("WELCOME! To Robots vs Dinosaurs!")
+
+  def battle (self): #void
+    pass
+
+  def dinosaur_turn (self, dinosaur): #void
+    pass
+
+  def robot_turn (self, robot): #void
+    pass
+
+  def show_dinosaur_opponent_options (self): #void
+    pass
+
+  def show_robot_opponent_options (self): #void
+    pass
+
+  def display_winners (self): #void
+    pass
+  
