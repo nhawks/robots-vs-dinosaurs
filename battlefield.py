a6a88a9d79c6ba0680ae2e81f7e7d9c8b38fe918
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-09 21:33:17 -0400

    minor improvements to comments and console readability

--- battlefield.py
+++ battlefield.py
@@ -7,11 +7,17 @@ class Battlefield:
   def __init__ (self):
     self.fleet = Fleet()
     self.herd = Herd()
-    self.run_game()
+    self.run_game() #starts game everytime Battlefield() is called 
 
   def run_game(self): #void
+    
+    #Start the game with a welcome msg
     self.display_welcome()
+
+    #run battle while loop until a team loses
     self.battle()
+
+    #once team loses end the game and display the winners
     self.display_winners()
 
   def display_welcome (self): #void
@@ -23,71 +29,71 @@ class Battlefield:
         self.selected_dinosaur(), 
       if self.fleet.robots:
         self.selected_robot()
-      # self.choose_attacking_team()
-      # print("\nTHE HERD\n")
-      # self.selected_dinosaur()
-      # if len(self.fleet.robots) > 0:
-      #   print("\nTHE FLEET\n")
-      #   self.selected_robot()
-
+ 
+  #methods that displays the current team members that a user can pick
   def selected_dinosaur (self):
-    print("\nTHE HERD\n")
+    print("\nTHE HERD: Attacking - Choose your Dinosaur!\n")
     self.show_dinosaur_opponent_options()
-    fighter = int(input("\nPlease enter one number to select the fighter you'd like to choose: "))
+    fighter = int(input("\nPlease enter a number to select the dinosaur you'd like to choose: "))
     self.dinosaur_turn(fighter)
 
   def selected_robot (self):
-    print("\nTHE FLEET\n")
+    print("\nTHE FLEET: Attacking - Choose your Robot!\n")
     self.show_robot_opponent_options()
-    fighter = int(input("\nPlease enter one number to select the fighter you'd like to choose: "))
+    fighter = int(input("\nPlease enter a number to select the robot you'd like to choose: "))
     self.robot_turn(fighter)
 
+  #methods that attack the opposite team member by selection 
+  #then calls a method to determine if opponenet's health > 0
   def dinosaur_turn (self, dinosaur): #void
     dinosaur = self.herd.dinosaurs[dinosaur]
-    print(f"You chose: {dinosaur.dinosaur_name}\n")
+    print(f"You chose: {dinosaur.dinosaur_name.upper()}\n")
+    print("THE FLEET: Defending - Choose your opponent! \n")
     self.show_robot_opponent_options()
-    opponent_index = int(input("\nPlease select one number to select your opponent: "))
+    opponent_index = int(input("\nPlease enter a number to select your opponent: "))
     opponent = self.fleet.robots[opponent_index]
-    print(f"\n{dinosaur.dinosaur_name} vs. {opponent.robot_name}")
+    print(f"\n{dinosaur.dinosaur_name.upper()} vs. {opponent.robot_name.upper()}")
     dinosaur.attack(opponent)
     self.check_robot_health(opponent, dinosaur, opponent_index)
     #TODO: add optiont to select the fighter's weapon
 
   def robot_turn (self, robot): #void
     robot = self.fleet.robots[robot]
-    print(f"You chose: {robot.robot_name}\n")
+    print(f"You chose: {robot.robot_name.upper()}\n")
+    print("THE HERD: Defending - Choose your opponent!\n")
     self.show_dinosaur_opponent_options()
-    opponent_index = int(input("Please select one number to select your opponent: "))
+    opponent_index = int(input("Please enter a number to select your opponent: "))
     opponent = self.herd.dinosaurs[opponent_index]
-    print(f"\n{robot.robot_name} vs. {opponent.dinosaur_name}")
+    print(f"\n{robot.robot_name.upper()} vs. {opponent.dinosaur_name.upper()}")
     robot.attack(opponent)
     self.check_dino_health(opponent, robot, opponent_index)
     #TODO: add optiont to select the fighter's weapon
 
+  #methods that display the current robots/dinosaurs and their health
   def show_dinosaur_opponent_options (self): #void
     for dinosaur in self.herd.dinosaurs:
       print(f"{self.herd.dinosaurs.index(dinosaur)} - {dinosaur.dinosaur_name} | Health: {dinosaur.dinosaur_health}/10")
 
   def show_robot_opponent_options (self): #void
     for robot in self.fleet.robots:
-      print(f"{self.fleet.robots.index(robot)} - {robot.robot_name} | Health: {robot.robot_health}")
+      print(f"{self.fleet.robots.index(robot)} - {robot.robot_name} | Health: {robot.robot_health}/10")
 
-  #methods to determine if robot/dinosaur health > 0, if not remove from game (list)
+  #methods to determine if robot/dinosaur health > 0, if not remove from game (del list[i])
   def check_dino_health(self, dinosaur, robot, dino_index):
     self.dino_index = dino_index
     if dinosaur.dinosaur_health <= 0:
       del self.herd.dinosaurs[dino_index]
-      print(f"{robot.robot_name} defeated {dinosaur.dinosaur_name}")
+      print(f"\n{robot.robot_name.upper()} DEFEATED {dinosaur.dinosaur_name.upper()}!")
   
   def check_robot_health(self, robot, dinosaur, robot_index):
     self.robot_index = robot_index
     if robot.robot_health <= 0:
       del self.fleet.robots[robot_index]
-      print(f"{dinosaur.dinosaur_name} defeated {robot.robot_name}")
+      print(f"\n{dinosaur.dinosaur_name.upper()} DEFEATED {robot.robot_name.upper()}!")
 
   #runs if the herd or fleet len(list) == 0
   def display_winners (self): #void
     if len(self.fleet.robots) == 0:
-      print("THE HERD WINS: The fleet was defeated!")
+      print("\nTHE HERD WINS: The Fleet was defeated!\nGAME OVER!")
     elif len(self.herd.dinosaurs) == 0:
-      print("THE FLEET WINS: The herd was defeated!")
+      print("\nTHE FLEET WINS: The Herd was defeated!\nGAME OVER!")
