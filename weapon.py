78ad6e52cd3e213f442eed104f2d9b4459acd508
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-10 03:04:53 -0400

    updated file so robot can select a weapon

--- weapon.py
+++ weapon.py
@@ -1,13 +1,12 @@
-from os import waitpid
-
-
 class Weapon:
+
+
   def __init__ (self, weapon_name):
       self.weapon_name = weapon_name
       self.weapon_damage = self.get_weapon_damage(weapon_name)
-
   
   def get_weapon_damage (self, weapon_name):
+    # self.weapon_name
     if weapon_name == "sword":
       weapon_damage = 2
       return weapon_damage
@@ -15,5 +14,6 @@ class Weapon:
       weapon_damage = 3
     elif weapon_name == "handcannon":
       weapon_damage = 4
-  
-  #TODO: create list of different weapons: [sword, laser, handcannon]
+    else:
+      weapon_damage = 0
+    return weapon_damage
