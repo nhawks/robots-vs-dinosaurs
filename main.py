34755c32844e0ee9dfd55a0257d9b908fb93d3af
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-09 21:01:38 -0400

    run one game or multiple games

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from battlefield import Battlefield
 
-
+#Run One Game
 if __name__ == "__main__":
   Battlefield()
+
+#Run Multiple Games
+# game_one = Battlefield()
+# game_two = Battlefield()
+# game_three = Battlefield()
