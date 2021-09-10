5f2a9a6ef089f3ff8226639021e10b201a2a6a13
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-10 03:47:00 -0400

    fixed file to allow dinosaur to select attack

--- dinosaur.py
+++ dinosaur.py
@@ -2,6 +2,7 @@ class Dinosaur:
 
   def __init__ (self, dinosaur_name, attack_power):
     self.dinosaur_name = dinosaur_name
+    self.attack_name = ""
     self.attack_power = attack_power
     self.dinosaur_health = 10
     self.dinosaur_energy_level = 100
@@ -11,7 +12,7 @@ class Dinosaur:
     self.dinosaur_energy_level -= 10
     robot.robot_health -= self.attack_power
     print (
-f"{self.dinosaur_name} attacked {robot.robot_name}!\n\
-Dealing {self.attack_power} damage!\n\
+f"{self.dinosaur_name} attacked {robot.robot_name} with {self.attack_name.upper()}!\n\
+{self.attack_name.upper()} dealt {self.attack_power} damage!\n\
 {robot.robot_name}'s Health is now: {robot.robot_health}/10!"
 )
--- herd.py
+++ herd.py
@@ -3,6 +3,8 @@ from dinosaur import Dinosaur
 
 class Herd:
 
+  herd_attacks = ("headbutt", "tailwhip", "bite")
+
   def __init__ (self):
     self.dinosaurs = []
     self.create_herd()
@@ -10,9 +12,9 @@ class Herd:
   #create a herd of dinosaurs and append to self.dinosaurs
   def create_herd (self): #void
     #Dinosaur(name, attack_power)
-    dinosaur_one = Dinosaur("Pteranodon", 2)
-    dinosaur_two = Dinosaur("Cearadactylus", 4)
-    dinosaur_three = Dinosaur("Tropeognathus", 3)
+    dinosaur_one = Dinosaur("Pteranodon", 0)
+    dinosaur_two = Dinosaur("Cearadactylus", 0)
+    dinosaur_three = Dinosaur("Tropeognathus", 0)
 
     self.dinosaurs.append(dinosaur_one)
     self.dinosaurs.append(dinosaur_two)
